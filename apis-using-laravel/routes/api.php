26ee6fe36d77ffeccf3a1aba9cd9ea6a4905f865
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-09-30 23:59:13 +0300

    adding route for apis

--- apis-using-laravel/routes/api.php
+++ apis-using-laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/sorted/{string} ", [MainController::class, "sort"]);
+
+Route::get("/getplace/{num} ", [MainController::class, "getNumPlace"]);
+
+Route::get("/programer/{text} ", [MainController::class, "humanToProgramer"]);
+
+Route::get("/math/{expression} ", [MainController::class, "calculate"]);
