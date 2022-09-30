a1cf09bfc03321ef4a40ead473e34f30cc71a212
Ali Alrida Shouman <alishouman993980@gmail.com>
2022-09-30 22:47:20 +0300

    Creating a function getNumPlace for API that receives a number and returns each place value in the number

--- apis-using-laravel/app/Http/Controllers/MainController.php
+++ apis-using-laravel/app/Http/Controllers/MainController.php
@@ -21,16 +21,21 @@ class MainController extends Controller
         $nb = array();
         $res = array();
         sort($arr);
+        // willl save all lower and upper letter in different array
         for ($i = 0; $i < count($arr); $i++) {
 
+
+            // Checks  if its a lowerCase 
             if ($arr[$i] >= 'a' && $arr[$i] <= 'z') {
                 $lowerCase[] = $arr[$i];
             }
 
+            // Checks  if its a upperCase
             if ($arr[$i] >= 'A' && $arr[$i] <= 'Z') {
                 $upperCase[] = $arr[$i];
             }
 
+            // Checks  if its a number
             if (is_Numeric($arr[$i])) {
                 $nb[] = $arr[$i];
             }
@@ -65,5 +70,42 @@ class MainController extends Controller
             "sorted" => $string
         ]);
     }
-    
+
+
+    // API that recives a number $num and returns each place value in the number
+    function getNumPlace($num)
+    {
+        // Checks if its a number
+        if (is_numeric($num)) {
+            // get nb of digits we have
+            $nbOfDigits = strlen($num);
+            //as we count it as string if it is neg must remove 1, the size of "-" minus sign
+            if ($num < 0) {
+                $nbOfDigits--;
+            }
+            // get last digit
+            $mod = $num % 10;
+            // remove last digit from num
+            $num = (int)$num / 10;
+            // place of this digit  like tens hundred thousand etc..
+            $place = 1;
+            //will save results in array
+            $arr = array();
+            while ($nbOfDigits != 0) {
+                $arr[] = $mod * $place;
+                $nbOfDigits--;
+                $place = $place * 10;
+                $mod = $num % 10;
+                $num = (int)$num / 10;
+            }
+            return response()->json([
+                "num" => array_reverse($arr)
+            ]);
+        } else {
+            // if send string return error
+            return response()->json([
+                "num" => "must Enter numeric Value"
+            ]);
+        }
+    }
 }
